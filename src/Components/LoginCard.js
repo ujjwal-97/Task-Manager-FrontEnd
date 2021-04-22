e1f7561bf894807fdee464e419ddb842b87b865c
Ujjwal Gautam <39196584+ujm2099@users.noreply.github.com>
2021-04-23 00:00:03 +0530

    modified the login page

--- src/Components/LoginCard.js
+++ src/Components/LoginCard.js
@@ -1,48 +1,133 @@
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
-import { Box, TextField } from '@material-ui/core';
-import { Button } from '@material-ui/core';
-import { styled } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, styled } from '@material-ui/core/styles';
 
-const MyButton = styled(({ color, ...other }) => <Button {...other} />)({
-  background: (props) => 'linear-gradient(225deg, #3356AE 30%, #21CBF3 90%)',
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage:
+      'url(https://wallpaperaccess.com/download/1080p-minimalist-2248109)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light'
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+  form: {
+    width: '80%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const MyButton = styled(Button)({
+  background: 'linear-gradient(45deg, #3251AB 30%, #21CBF3 90%)',
   border: 0,
-  borderRadius: 3,
-  boxShadow: (props) => '0 3px 5px 2px rgba(33, 203, 243, .3)',
+  borderRadius: 50,
+  boxShadow: '0 3px 5px 2px rgba(33, 203, 243, .3)',
   color: 'white',
   height: 48,
   padding: '0 30px',
-  margin: 8,
 });
 
-function LoginCard() {
+export default function LoginCard() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <Paper elevation={5}>
-        <h3>
-          <strong>Sign In</strong>
-        </h3>
-        <form>
-          <Box display="flex" flexDirection="column" p={3}>
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid
+        item
+        xs={12}
+        sm={8}
+        md={5}
+        component={Paper}
+        elevation={6}
+        square
+        style={{ backgroundColor: '#F5F5F5' }}
+      >
+        <Paper className={classes.paper} elevation={1}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate>
             <TextField
-              id="outlined-textarea"
-              label="Your Email"
-              placeholder="task@domain.com"
               variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
             />
             <TextField
-              id="outlined-textarea"
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
               label="Password"
               type="password"
-              placeholder="Password"
-              variant="outlined"
+              id="password"
+              autoComplete="current-password"
             />
-            <MyButton>SIGN IN</MyButton>
-          </Box>
-        </form>
-      </Paper>
-    </div>
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <MyButton
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </MyButton>
+            <Grid container>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  <p>
+                    Don't have an account? <strong>Sign Up</strong>
+                  </p>
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </Paper>
+      </Grid>
+    </Grid>
   );
 }
-
-export default LoginCard;
